10c01bacbff2b106ca068ce50ea954fe28a981f6
Yu-seok Jeong <59636424+j961224@users.noreply.github.com>
2022-03-05 18:17:13 +0900

    Update transformers_with_bilstm.py

--- models/transformers_with_bilstm.py
+++ models/transformers_with_bilstm.py
@@ -41,10 +41,10 @@ class LSTM_Model(nn.Module):
         # BERT output= (16, 244, 1024) (batch, seq_len, hidden_dim)
         output= self.model(input_ids= input_ids, attention_mask= attention_mask)[0]
 
-        # LSTM last hidden, cell state shape : (2, 244, 1024) (num_layer, seq_len, hidden_size)
+        # LSTM last hidden, cell state shape : (2, 244, 1024) (num_layer, seq_len, hidden_size) -> 2개 layer
         hidden, (last_hidden, last_cell)= self.lstm(output)
 
-        # (16, 1024) (batch, hidden_dim)
+        # (16, 1024) (batch, hidden_dim) -> back and forward last hidden states concat
         cat_hidden= torch.cat((last_hidden[0], last_hidden[1]), dim= 1)
         logits= self.fc(cat_hidden)
         
